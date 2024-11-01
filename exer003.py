079978048d0a580fb307f0378bce5ad3d5a0127a
WellingtonPereiraLuiz <wellingtonpereiraluiz89@gmail.com>
2024-10-31 20:32:28 -0400

    novo

---
+++ exer003.py
@@ -0,0 +1,11 @@
+n = input("Quer digitar quantos numeros?")
+
+for i in range(n):
+    valor = input(f"\nDigite o {i} valor:")
+    if (valor % valor == 0):
+        print
+
+
+
+git config --global user.email "wellingtonpereiraluiz89@gmail.com"
+git config --global user.name "WellingtonPereiraLuiz"
